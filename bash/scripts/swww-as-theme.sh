0d8229358da11d65c666a3bbf73fed64c19b3a5d
0lswitcher <manjarodistro@gmail.com>
2025-08-03 14:50:50 -0500

    Delete bash directory

--- bash/scripts/swww-as-theme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -f "$HOME/.cache/current-swww-img" ]; then
-    theme=$(<"$HOME/.cache/current-swww-img")
-    wal -i "$theme"
-    echo "$theme" > ~/.cache/current-pywal-theme
-    bash /home/y2k/stuff/scripts/pywal-to-spicetify.sh
-fi
