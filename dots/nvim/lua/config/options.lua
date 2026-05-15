7f2a6393b3b1aed723eb0ecde9ff2f29312ac691
0lswitcher <manjarodistro@gmail.com>
2026-05-14 21:12:38 -0500

    New programs and updates to old
    added kando, nvim, and obsidian files.
    (nvim is running through lazyvim + neovide)
    updated waybar

---
+++ dots/nvim/lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.g.neovide_cursor_animation_length = 0.1
+vim.g.neovide_cursor_trail_size = 0.5
+
+vim.o.guifont = "JetBrainsMono Nerd Font:h12"
